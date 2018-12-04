363bb9aedb0c7231b23ae9a1eff4a269b907deb1
黎志贤 <920673805@qq.com>
2018-12-04 18:18:16 +0800

    商城服务端第一次
    忘记跟页面整合在一起了

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Headers', 'x-requested-with');
     next();
 })
 
--- routes/upLoad.js
+++ routes/upLoad.js
@@ -1,10 +1,28 @@
+var fs = require('fs');
 const router = require('express').Router();
 var multer = require('multer')
-var upload = multer({ dest: 'E:\GitHub Desktop Local\net_shop\server_shop\asset\imges' })
+var upload = multer({ dest: 'asset/imges/' })
 
 router.post('/', upload.any(), function (req, res) {
     console.log(req.files);
     console.log(req.body);
+
+    var des_file = 'asset/imges/' + req.files[0].originalname;
+    fs.readFile(req.files[0].path, function (err, data) {
+        fs.writeFile(des_file, data, function (err) {
+            if (err) {
+                console.log(err);
+            } else {
+                response = {
+                    message: 'File uploaded successfully',
+                    filename: req.files[0].originalname
+                };
+                console.log(response);
+                res.end(JSON.stringify(response));
+            }
+        });
+    });
+
     res.end('上传成功');
 })
 
